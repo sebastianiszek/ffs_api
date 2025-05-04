320cab6d505cc638d838f5bac0274aef0efa7ccc
sebastianiszek <sebastian.salata783@gmail.com>
2025-05-04 20:11:01 +0100

    chore: update for deployment

--- index.js
+++ index.js
@@ -38,7 +38,10 @@ app.listen(port, () => {
 
 const getPageContent = async () => {
     // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
 
     // Navigate the page to a URL.
