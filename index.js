1503a32b2e94e459eb1eb0049ad01d5dd4bdc07e
Vutulosa <59256117+Vutulosa@users.noreply.github.com>
2025-05-06 19:18:37 +0100

    chore: remove heroku path

--- index.js
+++ index.js
@@ -39,7 +39,6 @@ app.listen(port, () => {
 const getPageContent = async () => {
     // Launch the browser and open a new blank page
     const browser = await puppeteer.launch({
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH? process.env.PUPPETEER_EXECUTABLE_PATH : false,
         headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
